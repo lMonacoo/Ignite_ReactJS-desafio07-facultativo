9858eaeb5c0f4aa7fa7e4f4d62bd4c9f880faa77
lMonacoo <lucas.monaco.maquina@gmail.com>
2021-06-11 00:29:09 -0300

    :tada:

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>WorldTrip</title>
+      </Head>
+
+      <h1>Home</h1>
+    </>
+  );
+}
